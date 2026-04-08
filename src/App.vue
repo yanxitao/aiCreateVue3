a56c9107943e4c08da58443d52b3e13fdd57056d
yanxitao <136620970@qq.com>
2026-04-08 17:42:50 +0800

    Update App.vue to import and display JokeGenerator component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <JokeGenerator />
+  </div>
+</template>
+
+<script>
+import JokeGenerator from './components/JokeGenerator.vue';
+
+export default {
+  name: 'App',
+  components: {
+    JokeGenerator,
+  },
+};
+</script>
+
+<style>
+/* Add any necessary styles here */
+</style>
